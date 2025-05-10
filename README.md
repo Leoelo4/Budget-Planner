3f27399d577d29adc8d065d9a2cd520e325e4160
Leos <Leo.mills1709@gmail.com>
2025-05-10 21:44:03 +0100

    GUI Window, Header and currency selection feature

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Budget-Planner
-
-# Project Overview:
+# Project Overview
 This project provides a user-friendly Graphical User Interface (GUI) using Tkinter for users to manage their finance and budget. Users can input their income and expenses, categorise them (Food, utility etc...) and display a summarised breakdown of their budget.
 
 This project calculates a users total income, total expenses and remaining balance with a clear and well-thought design to display to users through Tkinter.
 
-# How To Use This Project:
+# How To Use This Project
 Incomplete Section
 
 # Summarised Information
--- main.py
+++ main.py
@@ -3,3 +3,42 @@
 # Importing necessary libraries
 import tkinter as tk
 from tkinter import ttk
+
+# Global constants 
+# Storing options for currency, income categories, and expense categories
+CURRENCY_OPTIONS = ["£", "$", "€"]
+INCOME_CATEGORIES = ["Salary", "Bonus", "Other"]
+EXPENSE_CATEGORIES = ["Food", "Rent", "Utilities", "Entertainment", "Other"]
+
+# Creating the main window
+root = tk.Tk()
+root.title("Budget Tracker GUI")                    
+root.geometry("800x700")
+root.configure(bg="#0d1b2a")
+root.minsize(800, 700)
+
+
+# Widget Styling
+style = ttk.Style(root)
+style.theme_use("clam")
+style.configure("TButton", font=("Arial", 10, "bold"))
+style.configure("TComboBox", font=("Arial", 10), padding=(2,2,2,2))
+
+# Header
+header = tk.Label(root, text="Budget Tracker GUI", font=("Arial", 24, "bold"), fg="white", bg="#0d1b2a")
+header.pack(pady=(10, 0))
+line_below_header = tk.Frame(root, height=2, width=700, bg="#778899")
+line_below_header.pack(pady=(5, 10))
+
+# Currency Selection Function
+currency_var = tk.StringVar(value=CURRENCY_OPTIONS[0])
+currency_frame = tk.Frame(root, bg="#0d1b2a")
+currency_frame.pack(pady=5)
+
+tk.Label(currency_frame, text="SELECT CURRENCY:", font=("Arial", 12, "bold"), fg="white", bg="#0d1b2a").pack(side=tk.LEFT, padx=5)
+
+currency_menu = ttk.Combobox(currency_frame, textvariable=currency_var, values = CURRENCY_OPTIONS, width=5, state="readonly")
+currency_menu.pack(side=tk.LEFT)
+ 
+
+root.mainloop()
